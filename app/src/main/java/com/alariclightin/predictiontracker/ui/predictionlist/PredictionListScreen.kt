3cca60e7c71eef1a84885e103e8ad36b694e562d
Alexander Biderman <alexanderbiderman@gmail.com>
2023-03-09 18:36:01 +0200

    Deleting unnecessary test tag

--- app/src/main/java/com/alariclightin/predictiontracker/ui/predictionlist/PredictionListScreen.kt
+++ app/src/main/java/com/alariclightin/predictiontracker/ui/predictionlist/PredictionListScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -97,7 +96,6 @@ fun PredictionListBody(
             Text(
                 text = stringResource(R.string.no_predictions_description),
                 style = MaterialTheme.typography.titleSmall,
-                modifier = modifier.testTag("NoPredictionTag")
             )
         }
     }
